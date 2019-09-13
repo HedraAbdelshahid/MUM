248a700a983ed7e07c5317e578c28e5943ea59eb
HedraAbdelshahid <55264668+HedraAbdelshahid@users.noreply.github.com>
2019-09-12 20:21:39 -0500

    first time committed

---
+++ Myclass.java
@@ -0,0 +1,20 @@
+package Q2;
+
+/*
+ * public class Myclass {
+ * 
+ * Myclass(){ ThesubClass sub=new ThesubClass();
+ * System.out.println("Myclass con"); } public static void main(String[] args) {
+ * new ThesubClass();
+ * 
+ * } }
+ * 
+ * private int value =4; Myclass(){ MyNestedClass n=new MyNestedClass();
+ * System.out.println(n.compute()); }
+ * 
+ * class ThesubClass extends Myclass { ThesubClass(){
+ * System.out.println("ThesubClass con"); }
+ * 
+ * }
+ * 
+ */
